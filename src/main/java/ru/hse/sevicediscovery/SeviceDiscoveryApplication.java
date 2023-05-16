e11724036bd44909abe3519d4d791a17571fddbe
Night River <nightdevgame@gmail.com>
2023-05-16 03:25:21 +0300

    init commit

---
+++ src/main/java/ru/hse/sevicediscovery/SeviceDiscoveryApplication.java
@@ -0,0 +1,15 @@
+package ru.hse.sevicediscovery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class SeviceDiscoveryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SeviceDiscoveryApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ru/hse/sevicediscovery/SeviceDiscoveryApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.hse.sevicediscovery;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SeviceDiscoveryApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
